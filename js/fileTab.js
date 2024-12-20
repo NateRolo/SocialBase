0b21f879519a5b9860ac706405b1f16e15e2b9f1
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2024-12-19 17:14:36 -0800

    added chevron and functionality
    added unique id's per accordian
    
    added archive icons, still positioning

--- js/fileTab.js
+++ js/fileTab.js
@@ -1,14 +1,27 @@
 var accordians = document.getElementsByClassName("accordian");
+var downChevrons = document.getElementsByClassName("chevronDown");
+var upChevrons = document.getElementsByClassName("chevronUp");
 
+
+// Open panel if accordian clicked
 for (let i = 0; i < accordians.length; i++){
     accordians[i].addEventListener("click", function() {
         var panel = this.nextElementSibling;
         panel.classList.toggle("active");
-        if (panel.style.maxHeight) {
-            panel.style.maxHeight = null;
-        } else {
-            panel.style.maxHeight = panel.scrollHeight + "px";
-        }
+        
+        // toggle downChevron
+        var downChevron = downChevrons[i];
+        downChevron.classList.toggle("hide");
+
+        // toggle upChevron
+        var upChevron = upChevrons[i];
+        upChevron.classList.toggle("hide");
     })
 };
+
+// Assign unique id per accordian
+for (let i = 0; i < accordians.length; i++){
+    accordians[i].setAttribute("id", "accordian" + i);
+}
+
 

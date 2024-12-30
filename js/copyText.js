651a6e828cd909521a16bd2ebe6f534961b8bf3e
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2024-12-29 20:05:29 -0800

    refactored so that initializing variables is a function that gets called after populateAccordian()
    refactored all code assigning id's to repsective elements so they are now functions that get called by initalizeVariables()
    
    copyText.js is now modular and exports functions to accordian.js

--- js/copyText.js
+++ js/copyText.js
@@ -1,39 +1,48 @@
-// subPanels
-var igSubPanels = document.querySelectorAll(".subPanel.instagram");
-var ytSubPanels = document.querySelectorAll(".subPanel.youtube");
-var ttSubPanels = document.querySelectorAll(".subPanel.tiktok");
-var fbSubPanels = document.querySelectorAll(".subPanel.facebook");
+export async function initializeVariables() {
+    // subPanels
+    var igSubPanels = document.querySelectorAll(".subPanel.instagram");
+    var ytSubPanels = document.querySelectorAll(".subPanel.youtube");
+    var ttSubPanels = document.querySelectorAll(".subPanel.tiktok");
+    var fbSubPanels = document.querySelectorAll(".subPanel.facebook");
 
-// captions
-var igCaptions = document.querySelectorAll(".caption.info.instagram");
-var ytCaptions = document.querySelectorAll(".caption.info.youtube");
-var ttCaptions = document.querySelectorAll(".caption.info.tiktok");
-var fbCaptions = document.querySelectorAll(".caption.info.facebook");
+    // captions
+    var igCaptionArray = document.querySelectorAll(".caption.info.instagram");
+    var ytCaptions = document.querySelectorAll(".caption.info.youtube");
+    var ttCaptions = document.querySelectorAll(".caption.info.tiktok");
+    var fbCaptions = document.querySelectorAll(".caption.info.facebook");
 
-//hashtags
-var igHashtags = document.querySelectorAll(".hashtags.info.instagram");
-var ytHashtags = document.querySelectorAll(".hashtags.info.youtube");
-var ttHashtags = document.querySelectorAll(".hashtags.info.tiktok");
-var fbHashtags = document.querySelectorAll(".hashtags.info.facebook");
+    //hashtags
+    var igHashtags = document.querySelectorAll(".hashtags.info.instagram");
+    var ytHashtags = document.querySelectorAll(".hashtags.info.youtube");
+    var ttHashtags = document.querySelectorAll(".hashtags.info.tiktok");
+    var fbHashtags = document.querySelectorAll(".hashtags.info.facebook");
 
-// subPanel buttons
-var igCopy = document.querySelectorAll(".copy.instagram");
-var ytCopy = document.querySelectorAll(".copy.youtube");
-var ttCopy = document.querySelectorAll(".copy.tiktok");
-var fbCopy = document.querySelectorAll(".copy.facebook");
+    // subPanel buttons
+    var igCopy = document.querySelectorAll(".copy.instagram");
+    var ytCopy = document.querySelectorAll(".copy.youtube");
+    var ttCopy = document.querySelectorAll(".copy.tiktok");
+    var fbCopy = document.querySelectorAll(".copy.facebook");
 
-// accordians
-var accordians = document.getElementsByClassName("accordian");
-var pageElements = accordians.length;
+    // accordians
+    var accordians = document.getElementsByClassName("accordian");
+    var pageElements = accordians.length;
 
-// buttons
-var igButtons = document.querySelectorAll(".socials.instagram");
-var ytButtons = document.querySelectorAll(".socials.youtube");
-var fbButtons = document.querySelectorAll(".socials.facebook");
-var ttButtons = document.querySelectorAll(".socials.tiktok");
+    // buttons
+    var igButtons = document.querySelectorAll(".socials.instagram");
+    var ytButtons = document.querySelectorAll(".socials.youtube");
+    var fbButtons = document.querySelectorAll(".socials.facebook");
+    var ttButtons = document.querySelectorAll(".socials.tiktok");
 
+    console.log("igCaptions:", igCaptionArray);
+
+    assignSubPanelId(pageElements, igSubPanels, ytSubPanels, ttSubPanels, fbSubPanels);
+    assignCaptionDivId(pageElements, igCaptionArray);
+    assignHashtagDivId(pageElements, igHashtags);
+    assignButtonId(pageElements, igCopy);
+    addCopyTextFunction(pageElements, igCaptionArray, igHashtags, igCopy);
+}
 // copy function
-async function copyText(text) {
+export async function copyText(text) {
     try {
         await navigator.clipboard.writeText(text);
         console.log("Text copied");
@@ -44,94 +53,98 @@ async function copyText(text) {
 }
 
 // Assign id to each subPanel
-for (let i = 0; i < pageElements; i++){
-    igSubPanels[i].setAttribute("id", "igSubPanel-" + i);
-    ytSubPanels[i].setAttribute("id", "ytSubPanel-" + i);
-    ttSubPanels[i].setAttribute("id", "ttSubPanel-" + i);
-    fbSubPanels[i].setAttribute("id", "fbSubPanel-" + i);
+export function assignSubPanelId(pageElements, igSubPanels, ytSubPanels, ttSubPanels, fbSubPanels) {
+    for (let i = 0; i < pageElements; i++) {
+        igSubPanels[i].setAttribute("id", "igSubPanel-" + i);
+        ytSubPanels[i].setAttribute("id", "ytSubPanel-" + i);
+        ttSubPanels[i].setAttribute("id", "ttSubPanel-" + i);
+        fbSubPanels[i].setAttribute("id", "fbSubPanel-" + i);
+    }
 }
 
 // Assign id to each caption div
-for (let i = 0; i < pageElements; i++){
-    igCaptions[i].setAttribute("id", "igCaption-" + i);
-    // ytCaptions[i].setAttribute("id", "ytCaption-" + i);
-    // ttCaptions[i].setAttribute("id", "ttCaption-" + i);
-    // fbCaptions[i].setAttribute("id", "fbCaption-" + i);
-}
-
-// Assign id to each hashtag div
-for (let i = 0; i < pageElements; i++){
-    igHashtags[i].setAttribute("id", "igHashtag-" + i);
-    // ytHashtag[i].setAttribute("id", "ytHashtag-" + i);
-    // ttHashtag[i].setAttribute("id", "ttHashtag-" + i);
-    // fbHashtag[i].setAttribute("id", "fbHashtag-" + i);
-}
- 
-// Assign id to each button 
-for (let i = 0; i < pageElements; i += 3) {
-    igCopy[i].setAttribute("id", "igCopyCaption-" + i);
-    igCopy[i + 1].setAttribute("id", "igCopyHashtags-" + i);
-    igCopy[i + 2].setAttribute("id", "igCopyAll-" + i);
-}
-
-// Assign copy text buttons (instagram only)
-for (let i = 0; i < pageElements; i++) {
-    let thisCaption = document.getElementById("igCaption-" + i).textContent;
-    let thisHashtag = document.getElementById("igHashtag-" + i).textContent;
-    let allData = thisCaption + "\n" + thisHashtag;
-
-    let copyCaptionButton = document.getElementById("igCopyCaption-" + i);
-    copyCaptionButton.addEventListener("click", () => 
-        copyText(thisCaption));
-
-    let copyHashtagButton = document.getElementById("igCopyHashtags-" + i);
-    copyHashtagButton.addEventListener("click", () =>
-        copyText(thisHashtag));
-
-    let copyAllButton = document.getElementById("igCopyAll-" + i);
-    copyAllButton.addEventListener("click", () =>
-        copyText(allData));
-
-}
-
-
-
-// alert function for buttons
-function buttonAlert(platform) {
-    var copiedPlatform;
-    switch (platform) {
-        case "Instagram":
-            copiedPlatform = "Instagram";
-            break;
-        case "Youtube":
-            copiedPlatform = "Youtube";
-            break;
-        case "Facebook":
-            copiedPlatform = "Facebook";
-            break;
-        case "TikTok":
-            copiedPlatform = "TikTok";
-            break;
-        default: 
-            alert("No text was copied");
+export function assignCaptionDivId(pageElements, igCaptions) {
+    for (let i = 0; i < pageElements; i++) {
+        igCaptions[i].setAttribute("id", "igCaption-" + i);
+        // ytCaptions[i].setAttribute("id", "ytCaption-" + i);
+        // ttCaptions[i].setAttribute("id", "ttCaption-" + i);
+        // fbCaptions[i].setAttribute("id", "fbCaption-" + i);
     }
-    alert(copiedPlatform + " metadata was copied!");
+}
+// Assign id to each hashtag div
+export function assignHashtagDivId(pageElements, igHashtags) {
+    for (let i = 0; i < pageElements; i++) {
+        igHashtags[i].setAttribute("id", "igHashtag-" + i);
+        // ytHashtag[i].setAttribute("id", "ytHashtag-" + i);
+        // ttHashtag[i].setAttribute("id", "ttHashtag-" + i);
+        // fbHashtag[i].setAttribute("id", "fbHashtag-" + i);
+    }
 }
 
-// Social media platform event listeners
-for (let i = 0; i < pageElements; i++) {
-    igButtons[i].addEventListener("click", function () {
-        buttonAlert("Instagram");
-    })
-    ytButtons[i].addEventListener("click", function () {
-        buttonAlert("Youtube");
-    })
-    fbButtons[i].addEventListener("click", function () {
-        buttonAlert("Facebook");
-    })
-    ttButtons[i].addEventListener("click", function () {
-        buttonAlert("TikTok");
-    })
+// Assign id to each button 
+export function assignButtonId(pageElements, igCopy) {
+    for (let i = 0; i < pageElements; i += 3) {
+        igCopy[i].setAttribute("id", "igCopyCaption-" + i);
+        igCopy[i + 1].setAttribute("id", "igCopyHashtags-" + i);
+        igCopy[i + 2].setAttribute("id", "igCopyAll-" + i);
+    }
 }
+// Assign copy text buttons (instagram only)
+export function addCopyTextFunction(pageElements, igCaptions, igHashtags, igCopy) {
+    for (let i = 0; i < pageElements; i++) {
+        let thisCaption = document.getElementById("igCaption-" + i).textContent;
+        let thisHashtag = document.getElementById("igHashtag-" + i).textContent;
+        let allData = thisCaption + "\n" + thisHashtag;
+
+        let copyCaptionButton = document.getElementById("igCopyCaption-" + i);
+        copyCaptionButton.addEventListener("click", () =>
+            copyText(thisCaption));
+
+        let copyHashtagButton = document.getElementById("igCopyHashtags-" + i);
+        copyHashtagButton.addEventListener("click", () =>
+            copyText(thisHashtag));
+
+        let copyAllButton = document.getElementById("igCopyAll-" + i);
+        copyAllButton.addEventListener("click", () =>
+            copyText(allData));
+    }
+}
+// alert function for buttons
+// function buttonAlert(platform) {
+//     var copiedPlatform;
+//     switch (platform) {
+//         case "Instagram":
+//             copiedPlatform = "Instagram";
+//             break;
+//         case "Youtube":
+//             copiedPlatform = "Youtube";
+//             break;
+//         case "Facebook":
+//             copiedPlatform = "Facebook";
+//             break;
+//         case "TikTok":
+//             copiedPlatform = "TikTok";
+//             break;
+//         default:
+//             alert("No text was copied");
+//     }
+//     alert(copiedPlatform + " metadata was copied!");
+// }
+
+// // Social media platform event listeners
+// export function addfor (let i = 0; i < pageElements; i++) {
+//     igButtons[i].addEventListener("click", function () {
+//         buttonAlert("Instagram");
+//     })
+//     ytButtons[i].addEventListener("click", function () {
+//         buttonAlert("Youtube");
+//     })
+//     fbButtons[i].addEventListener("click", function () {
+//         buttonAlert("Facebook");
+//     })
+//     ttButtons[i].addEventListener("click", function () {
+//         buttonAlert("TikTok");
+//     })
+// }
 
 

2be34808acd56c4a1602dd5bf813c6738010d1b8
Nathan Oloresisimo <146795346+NateRolo@users.noreply.github.com>
2024-12-29 20:06:39 -0800

    wrote code that now populates accordians with data from firestore

--- js/accordian.js
+++ js/accordian.js
@@ -1,5 +1,61 @@
 import { getFirestore, collection, doc, setDoc, getDocs, query, where } from 'https://www.gstatic.com/firebasejs/11.1.0/firebase-firestore.js';
 import { app } from './firebase_API.js';
+import { addCopyTextFunction, assignButtonId, assignHashtagDivId, assignSubPanelId, assignCaptionDivId, copyText, initializeVariables } from './copyText.js';
+
+// gets snapshot of entire collection 
+const db = getFirestore(app);
+const postsSnap = await getDocs(collection(db, "posts"));
+
+// adds objects to array 
+var postsArray = [];
+postsSnap.forEach((doc) => {
+    postsArray.push({
+        id: doc.id,
+        data: doc.data()
+    })
+})
+
+console.log(postsArray[0].data);
+
+// populate template with data from array
+async function populateAccordian(array) {
+    let postTemplate = document.getElementById("postsTemplate");
+
+    for (let i = 0; i < array.length; i++) {
+        let thisPost = array[i];
+        var fileName = thisPost.data.filename;
+        var filePosted = thisPost.data.datePosted;
+        var archivedStatus = thisPost.data.archived;
+        var igCaption = thisPost.data.igdata[0];
+        var igHashtags = thisPost.data.igdata[1];
+        var igViews = thisPost.data.igdata[2];
+        var igLikes = thisPost.data.igdata[3];
+        var igComments = thisPost.data.igdata[4];
+        var igShares = thisPost.data.igdata[5];
+
+        let newPost = postTemplate.content.cloneNode(true);
+
+        newPost.querySelector('.fileNumber').innerHTML = fileName;
+        newPost.querySelector('.fileNumber').setAttribute("id", fileName);
+        newPost.querySelector('.filePostedDate').innerHTML = filePosted;
+        if (archivedStatus) {
+            newPost.querySelector('.archive.button').classList.toggle('stored');
+        }
+        newPost.querySelector('.caption.info.instagram').insertAdjacentHTML("beforeend", igCaption);
+        newPost.querySelector('.hashtags.info.instagram').insertAdjacentHTML("beforeend", igHashtags);
+        newPost.querySelector('.views').insertAdjacentHTML("beforeend", igViews);
+        newPost.querySelector('.likes').insertAdjacentHTML("beforeend", igLikes);
+        newPost.querySelector('.comments').insertAdjacentHTML("beforeend", igComments);
+        newPost.querySelector('.shares').insertAdjacentHTML("beforeend", igShares);
+
+        document.getElementById('content').appendChild(newPost);
+    }
+
+}
+
+await populateAccordian(postsArray).then(() =>{
+    initializeVariables();
+});
 
 var accordians = document.getElementsByClassName("accordian");
 var downChevrons = document.getElementsByClassName("chevronDown");
@@ -49,43 +105,3 @@ for (let i = 0; i < pageElements; i++) {
         archiveButtons[i].classList.toggle("stored");
     })
 }
-
-// gets snapshot of entire collection 
-const db = getFirestore(app);
-const postsSnap = await getDocs(collection(db, "posts"));
-
-// adds objects to array 
-var postsArray = [];
-postsSnap.forEach((doc) => {
-    postsArray.push({
-        id: doc.id,
-        data: doc.data
-    })
-})
-
-console.log(postsArray[0].id);
-
-
-
-
-
-// populate information into accordian
-
-
-// populate likes to appropriate panel
-// async function populateLikes() {
-// try {
-//     const likesSection = document.getElementsByClassName("likes");
-//     const doc = await getDoc(docRef);
-//     console.log("Cached document data:", doc.data());
-//         for (let i = 0; i < likesSection.length; i++) {
-//             likesSection[i].insertAdjacentHTML("beforeend", doc.data().likes);
-//         }
-//     }
-//     catch (e) {
-//         console.log("Error: ", e);
-//     }
-// };
-
-// populateLikes();
-
